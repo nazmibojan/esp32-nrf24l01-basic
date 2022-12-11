96b136c54cf9f4f1fda0dca6a37ff762a6ed15eb
Nazmi Febrian <nazmi.febrian@pitik.id>
2022-12-11 14:05:37 +0700

    Change tx method and rx address

--- nrf24l01-basic-comm/src/main.cpp
+++ nrf24l01-basic-comm/src/main.cpp
@@ -2,14 +2,21 @@
 #include <SPI.h>
 #include <RF24.h>
 
-RF24 radio(4, 5);
-uint8_t txAddress[6] = "1Node";
+// #define TX_MODE
 
-bool radioNumber;
-bool role = false;  // true = TX node, false = RX node
+RF24 radio(4, 5);
+
 uint8_t const payloadSize = 5;
+
+#ifdef TX_MODE
+uint8_t txAddress[6] = "1Node";
 uint8_t bufferTx[payloadSize + 1] = "12345";
 uint16_t failuresTx = 0;
+#else
+uint8_t rxAddress[6] = "1Node";
+char bufferRx[payloadSize + 1] = "";
+uint16_t counterRx;
+#endif
 
 void setup() {
   Serial.begin(9600);
@@ -26,23 +33,40 @@ void setup() {
   radio.setPALevel(RF24_PA_LOW);
   radio.setPayloadSize(payloadSize);
 
+#ifdef TX_MODE
   radio.openWritingPipe(txAddress);
   radio.stopListening();
+#else
+  radio.openReadingPipe(0, rxAddress);
+  radio.startListening();
+  radio.printDetails();
+#endif
 }
 
 void loop() {
-  radio.flush_tx();
-  if (!radio.writeFast(&bufferTx, payloadSize)) {
-    Serial.println(F("Failed to transmit data to receiver"));
-    failuresTx++;
-    radio.reUseTX();
-  } else {
-    Serial.println(F("Successfully transmit data to receiver"));
-  }
+#ifdef TX_MODE
+  // radio.flush_tx();
+  // if (!radio.writeFast(&bufferTx, payloadSize)) {
+  //   Serial.println(F("Failed to transmit data to receiver"));
+  //   failuresTx++;
+  //   radio.reUseTX();
+  // } else {
+  //   Serial.println(F("Successfully transmit data to receiver"));
+  // }
 
-  if (failuresTx >= 100) {
-    Serial.println(F("Too many failures detected"));
-  }
+  // if (failuresTx >= 100) {
+  //   Serial.println(F("Too many failures detected"));
+  // }
+  radio.write(&bufferTx, sizeof(bufferTx));
 
   delay(5000);
+#else
+  if (radio.available()) {
+    radio.read(bufferRx, payloadSize);
+    Serial.print(F("Received Data: "));
+    Serial.print(bufferRx);
+    Serial.print(F(" - "));
+    Serial.println(counterRx++);
+  }
+#endif
 }
